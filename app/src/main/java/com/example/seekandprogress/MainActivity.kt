a0e2e2696f5e8ce82f9c31fbd147f11fc333a914
Full Name <fpawelec0@gmail.com>
2023-02-10 16:09:35 +0100

    writing some code

--- app/src/main/java/com/example/seekandprogress/MainActivity.kt
+++ app/src/main/java/com/example/seekandprogress/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.seekandprogress
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val img1 = findViewById<ImageView>(R.id.imageView3)
+    val img2 = findViewById<ImageView>(R.id.imageView4)
+    val img3 = findViewById<ImageView>(R.id.imageView6)
+    val btn = findViewById<Button>(R.id.button)
+
+    var pgb = findViewById<ProgressBar>(R.id.progressBar)
+    var pgb2 = findViewById<ProgressBar>(R.id.progressBar3)
+
+
 }
